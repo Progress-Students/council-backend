b5fe59ad785fcdbd748cbf0376dffb1125ca80fe
formalParrot <a.e.makarov13@gmail.com>
2025-12-23 15:02:40 +0400

    huckle

--- scr/index.js
+++ scr/index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
 
+app.get("/huckle", (req, res) => {
+  res.json({ message: "Huckle's tiny secret route" });
+});
+
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
